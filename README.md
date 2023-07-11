1910f1023b0cde47e0d869fa77ba2e19546bfd49
JulienBez <julienbezancon@gmail.com>
2023-07-11 13:56:12 +0200

    deleted doctodocx

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # ArabicToTXM
 
 Write something here
+
+You need to install libssl too in order for the code to convert doc to docx. Just type :
+
+```
+sudo apt install libssl-dev
+```
--- scripts/docToDocx.py
+++
@@ -1,8 +0,0 @@
-import glob
-import subprocess
-from tqdm import tqdm
-
-def docToDocx():
-    for doc in tqdm(list(glob.iglob("data/doc/*.doc"))):
-        subprocess.call(['soffice', '--headless', '--convert-to', 'docx', '--outdir', 'data/doc/',  doc])
-        subprocess.call(['rm',doc])
