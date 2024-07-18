15d163ba31195c2a006c18deaf5e22539817b114
Marceau-h <git@marceau-h.fr>
2024-07-18 17:41:08 +0200

    xml2connlu

--- scripts/xml2connlu.py
+++ scripts/xml2connlu.py
@@ -1,15 +1,35 @@
 from pathlib import Path
 
 from bs4 import BeautifulSoup
+from bs4.element import Tag
 from tqdm.auto import tqdm
 
 from io import StringIO
 
 
 
-file = Path("/home/marceau/PycharmProjects/ArabicToTXM/output/CorpusKARAMED/")
-output = Path("/home/marceau/PycharmProjects/ArabicToTXM/output/CorpusConllU/")
+file = Path("/home/marceau/PycharmProjects/ArabicToTXM/output/CorpusRimane/")
+output = Path("/home/marceau/PycharmProjects/ArabicToTXM/output/CorpusConllU/CamelFusion/")
 output.mkdir(exist_ok=True, parents=True)
+
+
+tagset_file = Path("/home/marceau/PycharmProjects/ArabicToTXM/tagset.txt")
+
+with tagset_file.open("r") as f:
+    tagset = f.readlines()
+
+tagset = [[e[0].strip(), e[1].strip()] for e in [e.split("=", 1) for e in tagset]]
+tagset = {e[0]: e[1] for e in tagset}
+
+def replace(tag):
+    return tagset.get(tag.upper(), tagset.get(tag, tag.upper()))
+
+def from_w_to_xpos(w: Tag) -> str:
+    # part1 = ''.join((w["asp"], w["per"], w["gen"], w["num"]))
+    # part2 = ''.join((w["stt"], w["cas"], w["vox"], w["mod"]))
+    # return f"{w['pos']}{f'.{part1}.{part2}' if part1 or part2 else ''}".upper()
+    return f"{w['pos']}{f'.{w['asp']}{w['per']}{w['gen']}{w['num']}.{w['stt']}{w['vox']}' if any(w[k] for k in ["asp", "per", "gen", "num", "stt", "cas", "vox", "mod"]) else ''}".upper()
+
 
 
 for xml in tqdm(list(file.glob("*.xml"))):
@@ -18,11 +38,6 @@ for xml in tqdm(list(file.glob("*.xml"))):
 
     w_s = soup.find_all("w")
 
-    data = [
-        (i, w.text, w["pos"])
-        for i, w in enumerate(w_s)
-    ]
-
     sents = []
     sent = []
     prev = None
@@ -30,18 +45,18 @@ for xml in tqdm(list(file.glob("*.xml"))):
         if len(sent) > 1 and w.text.strip() not in "!؟.!?" and prev in "!؟.!?":
             sents.append(sent)
             sent = []
-        sent.append((w.text, w["pos"], w["atbtok"]))
+        sent.append((w.text.strip(), replace(w["pos"]), w["atbtok"], from_w_to_xpos(w)))
         prev = w.text.strip()
     if sent:
         sents.append(sent)
 
 
     connlu = StringIO()
-    connlu.write("# global.columns = ID FORM LEMMA UPOS XPOS FEATS HEAD DEPREL DEPS MISC\n")
+    connlu.write("# global.columns = ID FORM LEMMA UPOS XPOS FEATS HEAD DEPREL DEPS MISC\n\n")
     for i, sent in enumerate(sents, 1):
-        connlu.write(f"# sent_id = {i}\n# text = {' '.join(w for w, _, _ in sent)}\n")
-        for j, (w, pos, atbtok) in enumerate(sent, 1):
-            connlu.write(f"{j}\t{w}\t{atbtok}\t{pos}\t_\t_\t_\t_\t_\t_\n")
+        connlu.write(f"# sent_id = {i}\n# text = {' '.join(w for w, _, _, _ in sent)}\n")
+        for j, (w, pos, atbtok, xpos) in enumerate(sent, 1):
+            connlu.write(f"{j}\t{w}\t{atbtok}\t{pos}\t{xpos}\t_\t_\t_\t_\t_\n")
         connlu.write("\n")
 
     connlu.seek(0)
